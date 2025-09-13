30341d8e0a801ee4b5de6998eac8b424a8a964ea
The842 <123998630+The842@users.noreply.github.com>
2025-09-13 16:50:18 -0400

    Task 3 completed

--- Assets/Script/GunComponent.cs
+++ Assets/Script/GunComponent.cs
@@ -9,5 +9,21 @@ public class GunComponent : MonoBehaviour
     private float chargeTime = 0.0f;
     private bool isCharging = false;
 
-   
+   void Update()
+   {
+    if (Input.GetButtonUp("Fire1"))
+    {
+        shootBullet();
+    }
+   }
+
+   void shootBullet()
+   {
+    GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+    Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+    float bulletImpulse = bulletMaxImpulse;
+
+    rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.Impulse);
+   }
 }
